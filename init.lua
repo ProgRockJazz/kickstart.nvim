b265703d3f9e1079fb550d93794aa0163567e60d
paxman101 <paxmanrocks@gmail.com>
2025-06-22 18:50:45 -0700

    update init.lua

--- init.lua
+++ init.lua
@@ -40,12 +40,13 @@ What is Kickstart?
     reference for how Neovim integrates Lua.
     - :help lua-guide
     - (or HTML version): https://neovim.io/doc/user/lua-guide.html
+--]]
 
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = '\<space>'
-vim.g.maplocalleader = '\<space>'
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
